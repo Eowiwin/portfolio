0b66d1598f6f8e7513fd39f8711984b2d7512444
Erwan MENAGER <unconfigured@null.spigotmc.org>
2025-01-26 12:12:18 +0100

    Added GIF and reworked index

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
     var leftImage = document.querySelector('.left-img');
     var rightImage = document.querySelector('.right-img');
+    var leftGif = document.querySelector('.left-gif');
+    var rightGif = document.querySelector('.right-gif');
 
     function checkScroll() {
         var windowHeight = window.innerHeight;
         var leftImageTop = leftImage.getBoundingClientRect().top;
         var rightImageTop = rightImage.getBoundingClientRect().top;
-        if (leftImageTop < windowHeight * 0.5) {
+        var leftGifTop = leftGif.getBoundingClientRect().top;
+        var rightGifTop = rightGif.getBoundingClientRect().top;
+        if (leftImageTop < windowHeight * 0.7) {
             leftImage.classList.add('show-on-scroll');
         } else {
             leftImage.classList.remove('show-on-scroll');
         }
-        if (rightImageTop < windowHeight * 0.5) {
+        if (rightImageTop < windowHeight * 0.7) {
             rightImage.classList.add('show-on-scroll');
         } else {
             rightImage.classList.remove('show-on-scroll');
+        }
+        if (leftGifTop < windowHeight * 0.7) {
+            leftGif.classList.add('show-on-scroll');
+        } else {
+            leftGif.classList.remove('show-on-scroll');
+        }
+        if (rightGifTop < windowHeight * 0.7) {
+            rightGif.classList.add('show-on-scroll');
+        } else {
+            rightGif.classList.remove('show-on-scroll');
         }
     }
     checkScroll();
