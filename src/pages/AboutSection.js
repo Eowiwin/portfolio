452bb613bb917d7b1a15449f8d53c40027c55e60
Erwan MENAGER <erwan.menager@etu.uca.fr>
2025-03-31 14:58:05 +0200

    Added landscape mode

--- src/pages/AboutSection.js
+++ src/pages/AboutSection.js
@@ -79,7 +79,7 @@ const AboutSection = ({ onNavigate }) => {
         
         <div className="relative z-10 w-full h-full p-4 flex flex-row items-center justify-between">
           {/* Text content - made more compact */}
-          <div className="w-6/12 pr-4 h-full flex flex-col justify-center">
+          <div className="w-7/12 pr-4 h-full flex flex-col justify-center">
             <h1 className={`text-2xl font-bold mb-2 font-display ${isDarkMode ? 'text-white' : 'text-black'}`}>
               {t.aboutTitle}
             </h1>
@@ -98,9 +98,9 @@ const AboutSection = ({ onNavigate }) => {
             </div>
           </div>
 
-          {/* Image part - optimized for landscape */}
-          <div className="w-6/12 h-full flex items-center justify-center">
-            <div className="relative h-[90%] aspect-[3/4] max-w-[40vh]">
+          {/* Image part - optimized for landscape with reduced size */}
+          <div className="w-5/12 h-full flex items-center justify-center">
+            <div className="relative h-[70%] aspect-[3/4] max-w-[30vh]">
               {/* Current image */}
               <div 
                 className="relative w-full h-full overflow-hidden rounded-lg"
