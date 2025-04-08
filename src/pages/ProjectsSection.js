f52746f15e3fb1483c36130f6986eaec26e9eacb
Erwan MENAGER <erwan.menager@etu.uca.fr>
2025-04-08 10:07:04 +0200

    Modified update date for portfolio

--- src/pages/ProjectsSection.js
+++ src/pages/ProjectsSection.js
@@ -743,7 +743,7 @@ const ProjectsSection = ({ onNavigate }) => {
         ? (isFrench ? process.env.PUBLIC_URL + "/img/portfolio-preview-dark.png" : process.env.PUBLIC_URL + "/img/portfolio-preview-dark-en.png")
         : (isFrench ? process.env.PUBLIC_URL + "/img/portfolio-preview-light.png" : process.env.PUBLIC_URL + "/img/portfolio-preview-light-en.png"),
       link: "https://www.behance.net/gallery/223117437/Portfolio",
-      updatedAt: "2025-04-06"
+      updatedAt: "2025-04-08"
     },
     {
       id: 2,
